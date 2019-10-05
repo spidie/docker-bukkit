af2d79eb96423dee5f035f8ae3ddda4e993db97f
Steve Dalton <steve@refactor.com.au>
2019-10-05 11:28:46 +1000

    increased RAM and upgrade to 1.14.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,5 +21,5 @@ if [[ "$TRAVIS" = true ]]; then
     echo "stop" | java -jar /root/$1.jar
 else
     [ ! -f /data/server.properties ] || [ "${FORCE_CONFIG}" = "true" ] && python3 /root/configure.py
-    java -jar /root/$1.jar
+    java -Xms2G -Xmx6G -jar /root/$1.jar
 fi
